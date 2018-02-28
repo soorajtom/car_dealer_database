d880aec45c9e8f676fe967d6e8fc5ecb21c4bcc9
libin-n-g <libinngeorge@gmail.com>
2018-02-28 12:13:23 +0530

    check for emi availability
    10 triggers completed

--- triggers.sql
+++ triggers.sql
@@ -117,12 +117,30 @@ CREATE TRIGGER `emi_register_check`
 BEGIN
 DECLARE v_id INT;
 SET v_id = (SELECT vehicle_id FROM books WHERE cusomer_order_id = new.customer_order_id);
-	IF new.emi_id IN (SELECT emi_id FROM has_offer WHERE vehicle_id = v_id) THEN
+	IF new.emi_id NOT IN (SELECT emi_id FROM has_offer WHERE vehicle_id = v_id) THEN
 	   SIGNAL SQLSTATE VALUE '45000'
 	   SET MESSAGE_TEXT = '[table : registered] - emi_id entered is invalid for given vehicle';
 	END IF;
 END;//
 
+
+DROP TRIGGER IF EXISTS `emi_avalibility_check` //
+CREATE TRIGGER `emi_avalibility_check`
+	BEFORE INSERT
+	ON `customer_payment`
+	FOR EACH ROW
+BEGIN
+DECLARE v_id INT;
+SET v_id = (SELECT vehicle_id FROM books WHERE cusomer_order_id = new.order_id);
+    IF new.type = 'emi' THEN
+	IF NOT EXISTS (SELECT emi_id
+	       	      FROM has_offer
+	       	      WHERE vehicle_id = v_id) THEN
+	   SIGNAL SQLSTATE VALUE '45000'
+	   SET MESSAGE_TEXT = '[table : customer_payment] - emi plan does not exist for given vehicle';
+	END IF;
+    END IF;
+END;//
 
 DELIMITER ;
 -- show triggers \G;
