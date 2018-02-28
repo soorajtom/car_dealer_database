bdcb29097f3ee799f88aaa7535a19f734be955f9
Sooraj Tom <111501036@smail.iitpkd.ac.in>
2018-02-28 12:14:02 +0530

    Test data addition

--- insert_data.sql
+++ insert_data.sql
@@ -54,10 +54,10 @@ VALUES (1, 'Athul M A', '9494226565', 'athulma@gmail.com'),
 -- INSERTING DATA FOR MANAGER
 --
 INSERT INTO `manager` (`id`, `name`, `address`, `salary`, `gender`) 
-VALUES (1, 'Shivang Shukla', '214H, 32nd Street, Westland', '80000', 'male'),
-(2, 'Sidhanth Thalian', 'Pookodethu H, Kallara, Kottayam', '75000', 'male'),
-(3, 'Suresh Gopi', 'Lakshmi, Sasthamangalam, Kollam', '95000', 'male'),
-(4, 'Jimmy Kimmel', '6840 Hollywood Blvd, Los Angeles', '90000', 'male');
+VALUES (1, 'Shivang Shukla', '214H, 32nd Street, Westland', '200000', 'male'),
+(2, 'Sidhanth Thalian', 'Pookodethu H, Kallara, Kottayam', '175000', 'male'),
+(3, 'Suresh Gopi', 'Lakshmi, Sasthamangalam, Kollam', '195000', 'male'),
+(4, 'Jimmy Kimmel', '6840 Hollywood Blvd, Los Angeles', '190000', 'male');
 
 
 --
@@ -65,12 +65,12 @@ VALUES (1, 'Shivang Shukla', '214H, 32nd Street, Westland', '80000', 'male'),
 --
 INSERT INTO `employee` 
 (`id`, `name`, `address`, `salary`, `gender`, `age`, `dept`, `managed_by`, `policy_manager`, `policy_join_date`, `policy_renewal_date`) 
-VALUES (1, 'Seely Booth', '23A, BL street, Orleans', '61000', 'male', '35', 'broker', '1', '4', '2018-02-01', '2018-02-27'),
-(2, 'Temperance Brennan', '23A, BL street, Orleans', '80000', 'female', '31', 'admin', '1', '4', '2018-02-01', '2018-02-27'),
-(3, 'Sheldon Cooper', 'Newhauser, Pasadena, CA', '75000', 'male', '35', 'broker', '2', '3', '2017-02-01', '2018-02-27'),
-(4, 'Seely Booth', '23A, BL street, Orleans', '61000', 'male', '35', 'broker', '1', '4', '2018-02-01', '2018-02-27'),
-(5, 'Seely Booth', '23A, BL street, Orleans', '61000', 'male', '35', 'broker', '1', '4', '2018-02-01', '2018-02-27'),
-(6, 'Seely Booth', '23A, BL street, Orleans', '61000', 'male', '35', 'broker', '1', '4', '2018-02-01', '2018-02-27'),
+VALUES (1, 'Seely Booth', '23A, BL street, Orleans', '80000', 'male', '35', 'broker', '1', '1', '2016-02-01', '2018-03-27'),
+(2, 'Temperance Brennan', '23A, BL street, Orleans', '120000', 'female', '31', 'admin', '1', '1', '2016-02-01', '2018-03-27'),
+(3, 'Sheldon Cooper', 'Neuhauser, Houston, Texas', '75000', 'male', '35', 'broker', '2', '2', '2016-08-14', '2018-03-27'),
+(4, 'Sal Vulcano', '132A, Weighbridge, Rico', '112000', 'male', '35', 'auxiliary', '3', '1', '2016-06-13', '2018-03-27'),
+(5, 'Chris Pratt', 'R216, Malcom St., Uphill', '80000', 'male', '35', 'human_resource', '4', '2', '2017-02-12', '2019-03-27'),
+(6, 'Lisa Cuddy', 'A32, Licoln St., NewJearsy', '95000', 'female', '35', 'human_resource', '3', '1', '2017-01-24', '2019-03-27');
 
 
  --
@@ -83,3 +83,26 @@ VALUES (1, 'Seely Booth', '23A, BL street, Orleans', '61000', 'male', '35', 'bro
  ('Jason Bourne', '45', '1'),
  ('Matt Daemon', '58', '4'),
  ('Navaneeth A', '28', '3');
+
+--
+-- INSERTING DATA FOR SALARY TRANSACTIONS
+--
+INSERT INTO `salary_transaction` 
+(`transaction_id`, `bank`, `date`, `account_number`, `amount`) VALUES 
+('ACRAF23DB3C4', '0032', '2018-02-01', '1125689756523', '80000'),  -- 1
+('ACRAF23DB3D4', '0032', '2018-01-01', '1125689756523', '83000'),
+('ACRAF23DB3E4', '0032', '2017-12-01', '1125689756523', '90000'),
+('ACRAF23DB3F4', '0032', '2017-11-01', '1125689756523', '81000'),
+('ACRAF23DB3G4', '0032', '2017-10-01', '1125689756523', '85000'),
+
+('BCRAF23DB3C4', '0054', '2018-02-01', '2125689756523', '80000'), -- 5
+('BCRAF23DB3D4', '0054', '2018-01-01', '2125689756523', '83000'),
+('BCRAF23DB3E4', '0054', '2017-12-01', '2125689756523', '90000'),
+('BCRAF23DB3F4', '0054', '2017-11-01', '2125689756523', '81000'),
+('BCRAF23DB3G4', '0054', '2017-10-01', '2125689756523', '85000'),
+
+('CCRAF23DB3C4', '0035', '2018-02-01', '3125689756523', '120000'), -- 2
+('CCRAF23DB3D4', '0035', '2018-01-01', '3125689756523', '123000'),
+('CCRAF23DB3E4', '0035', '2017-12-01', '3125689756523', '130000'),
+('CCRAF23DB3F4', '0035', '2017-11-01', '3125689756523', '121000'),
+('CCRAF23DB3G4', '0035', '2017-10-01', '3125689756523', '125000');
