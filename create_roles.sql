21a011866850a3cbb1399664f1da3a8cbc9c4ae6
libin-n-g <libinngeorge@gmail.com>
2018-04-07 14:57:40 +0530

    adds procedure for creating users

--- create_roles.sql
+++ create_roles.sql
@@ -53,3 +53,97 @@ GRANT SELECT, UPDATE, DELETE, INSERT ON car_dealer.vehicle TO vendor_company_dea
 GRANT SELECT, UPDATE, DELETE, INSERT ON car_dealer.vehicle_vendor TO vendor_company_dealer;
 GRANT SELECT, UPDATE, DELETE, INSERT ON car_dealer.vendor_order TO vendor_company_dealer;
 GRANT SELECT ON car_dealer.dealer_vendor_transaction TO vendor_company_dealer;
+
+
+
+
+--
+-- CREATING USER
+--
+
+CREATE OR REPLACE FUNCTION get_username() RETURNS VARCHAR(20) 
+RETURN SUBSTRING_INDEX(USER(), '@', 1) ;
+
+DELIMITER //
+
+
+CREATE OR REPLACE PROCEDURE create_user( IN username VARCHAR(30),
+       	  	 	     IN `password` VARCHAR(30), IN role VARCHAR(30))
+BEGIN
+DECLARE stmt VARCHAR(100);
+prepare stmt FROM CONCAT('CREATE OR REPLACE USER ''', username, '''@''%'' IDENTIFIED BY ''',`password`, '''');
+execute stmt;
+DEALLOCATE PREPARE stmt;
+prepare stmt FROM CONCAT('GRANT ', role , ' TO ''', username, '''');
+execute stmt;
+DEALLOCATE PREPARE stmt;
+prepare stmt FROM CONCAT('SET DEFAULT ROLE ' , role,' FOR ''',  username, '''@''%''');
+execute stmt;
+DEALLOCATE PREPARE stmt;
+END //
+
+
+CREATE OR REPLACE PROCEDURE INITEMPLOYEE()
+BEGIN
+DECLARE n INT DEFAULT 0;
+DECLARE i INT DEFAULT 0;
+DECLARE username VARCHAR(30);
+DECLARE role VARCHAR(30);
+DECLARE _dept VARCHAR(20);
+SELECT COUNT(*) FROM employee INTO n;
+SET i = 0;
+WHILE i<n DO
+  SET username = (SELECT name FROM employee LIMIT i,1);
+  SET _dept = (SELECT dept FROM employee LIMIT i,1);
+  	IF (_dept = 'manager') THEN
+		SET role = 'manager'; 
+	END IF; 
+	IF (_dept = 'accountant') THEN
+		SET role = 'accountant';
+	END IF;
+	IF (_dept = 'broker') THEN
+		SET role = 'broker';
+	END IF;
+	IF (_dept = 'admin') THEN
+		SET role = 'DBA';
+	END IF;
+  CALL create_user(username, username, role);
+  SET i = i + 1;
+END WHILE;
+END //
+
+CREATE OR REPLACE PROCEDURE INIT_VEHICLE_DEALER()
+BEGIN
+DECLARE n INT DEFAULT 0;
+DECLARE i INT DEFAULT 0;
+DECLARE username VARCHAR(30);
+DECLARE role VARCHAR(30);
+SELECT COUNT(*) FROM vehicle_vendor INTO n;
+SET role = 'vendor_company_dealer';
+SET i = 0;
+WHILE i<n DO
+  SET username = (SELECT name FROM vehicle_vendor LIMIT i,1);
+  CALL create_user(username, username, role);
+  SET i = i + 1;
+END WHILE;
+END //
+
+
+CREATE OR REPLACE PROCEDURE INIT_INSURANCE_AGENT()
+BEGIN
+DECLARE n INT DEFAULT 0;
+DECLARE i INT DEFAULT 0;
+DECLARE username VARCHAR(30);
+DECLARE role VARCHAR(30);
+SELECT COUNT(*) FROM insurance_company INTO n;
+SET role = 'insurance_agent';
+SET i = 0;
+WHILE i<n DO
+  SET username = (SELECT name FROM insurance_company LIMIT i,1);
+  CALL create_user(username, username, role);
+  SET i = i + 1;
+END WHILE;
+END //
+
+DELIMITER ;
+
