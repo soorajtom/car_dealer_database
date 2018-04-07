8fcaffacfd3773c0c8b36fd95eca4cbb4975c38d
Sooraj Tom <soorajkandathil@gmail.com>
2018-04-07 09:44:17 +0530

    Contact page created

---
+++ frontend/contact.php
@@ -0,0 +1,109 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "car_dealer";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Dealerkar</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/heroic-features.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <style type="text/css">
+      footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 30px; /* Set the fixed height of the footer here */
+  line-height: 10px; /* Vertically center the text there */
+  background-color: #f5f5f5;
+}
+    </style>
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="/">Dealerkar - Walk in drive out</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="check_book.php">Check booking status</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="contact.php">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="dbedit.php">Admin Area</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+
+    <div class="container" style="padding-top: 20px;">
+      <div class="jumbotron">
+        <h1>Dealerkar. The best deals in town</h1>
+        <p>Welcome to Dealerkar. Book your car today!</p>
+      </div>
+      <div class="row">
+      <div class="col-md-7">
+        <h3>Contact us:</h3>
+        3110 Main St Ste 300<br>
+Santa Monica, CA, 90405-5354
+        <h4>Email us: </h4>
+        sales@dealerkar.com
+      </div>
+      <!-- <h2>Latest deals</h2> -->
+
+      <!-- Page Features -->
+      
+    </div>
+      <!-- /.row -->
+      
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-4 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Dealerkar 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
